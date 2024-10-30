e7dd917291a0867c8fd6b3b51c5e3067192deb8e
marcus.zhao <marcus.zhao@zennioptical.com>
2024-10-30 06:27:03 -0400

    Updates

---
+++ implementation/python_script/core_dist_auto.py
@@ -0,0 +1,45 @@
+import re
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+categories = ['CA','A0505']
+file_names = ["com-amazon.ungraph.txt.json", "amazon0505.txt.json"]
+
+data = []
+for n in file_names:
+    core_count = defaultdict(int)
+    filename = f'../{n}_output_run_1.txt'
+    pattern = re.compile(r"final core number of\s+(\d+)")
+    with open(filename, "r") as file:
+        for line in file:
+            match = pattern.search(line)
+            if match:
+                core_number = int(match.group(1))
+                core_count[core_number] += 1
+    temp_core = []
+    temp_count = []
+    for core_number, count in sorted(core_count.items()):
+        temp_core.append(core_number)
+        temp_count.append(count)
+
+    data.append((temp_core,temp_count))
+
+custom_ticks = [0, 100, 1000, 10000, 100000, 200000, 500000, 1000000]
+fig = plt.figure(1, figsize=(9,9))
+ax1  = fig.add_subplot(111)
+# Plotting the data
+for x, y in data:
+    ax1.plot(x, y)
+
+#ax1.yaxis.set_ticks(custom_ticks)
+#ax1.set_yscale('log', basex=10)
+#ax1.yaxis.set_ticklabels(custom_ticks)
+
+# Adding labels and title
+plt.xlabel('Core Numbers', fontsize=16)
+plt.ylabel('Number of Vertices', fontsize=16)
+plt.yscale('log')
+# Displaying the plot
+plt.legend(['CA', 'A0505'])  # Add legend for each line
+plt.savefig("core_number_distribution.svg", format="svg")
+plt.show()
